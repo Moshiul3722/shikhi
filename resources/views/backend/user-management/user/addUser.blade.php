738ddefeadcfdddbb5aa0070df6cfa8a15c4d37f
Moshiul <gazimoshiul@gmail.com>
2022-09-25 13:44:44 +0600

    user-verification

--- resources/views/backend/user-management/user/addUser.blade.php
+++ resources/views/backend/user-management/user/addUser.blade.php
@@ -27,51 +27,101 @@
                                                             <label for="name" class="form-label">Name</label>
                                                             <input type="text" class="form-control" name="name"
                                                                 id="name" placeholder="Enter your name">
+
+                                                                @error('name')
+                                                                <p class="mb-0"><small
+                                                                        class="text-danger fs-6">{{ $message }}</small></p>
+                                                            @enderror
                                                         </div>
                                                         <div class="col-md-6">
                                                             <label for="userName" class="form-label">User Name</label>
                                                             <input type="text" class="form-control" name="userName"
                                                                 id="userName" placeholder="Enter user name">
+
+                                                                @error('userName')
+                                                                <p class="mb-0"><small
+                                                                        class="text-danger fs-6">{{ $message }}</small></p>
+                                                            @enderror
                                                         </div>
                                                         <div class="col-md-6">
                                                             <label for="email" class="form-label">Email</label>
                                                             <input type="email" class="form-control" name="email"
                                                                 id="email" placeholder="Email">
+
+                                                                @error('email')
+                                                                <p class="mb-0"><small
+                                                                        class="text-danger fs-6">{{ $message }}</small></p>
+                                                            @enderror
                                                         </div>
                                                         <div class="col-md-6">
                                                             <label for="phone" class="form-label">Phone</label>
                                                             <input type="text" class="form-control" name="phone"
                                                                 id="phone" placeholder="Enter phone number">
+
+                                                                @error('phone')
+                                                                <p class="mb-0"><small
+                                                                        class="text-danger fs-6">{{ $message }}</small></p>
+                                                            @enderror
                                                         </div>
                                                         <div class="col-md-6">
                                                             <label for="password" class="form-label">Password</label>
                                                             <input type="password" class="form-control" name="password"
                                                                 id="password" placeholder="password">
+
+                                                                @error('password')
+                                                                <p class="mb-0"><small
+                                                                        class="text-danger fs-6">{{ $message }}</small></p>
+                                                            @enderror
                                                         </div>
                                                         <div class="col-md-6">
-                                                            <label for="conPassword" class="form-label">Confirm
+                                                            <label for="password_confirmation" class="form-label">Confirm
                                                                 Password</label>
-                                                            <input type="password" class="form-control" id="inputPassword4"
+                                                            <input type="password" class="form-control" id="password_confirmation" name="password_confirmation"
                                                                 placeholder="Confirm Password">
+
+                                                                @error('password_confirmation')
+                                                                <p class="mb-0"><small
+                                                                        class="text-danger fs-6">{{ $message }}</small></p>
+                                                            @enderror
                                                         </div>
 
 
                                                         <div class="col-md-6">
                                                             <label for="inputState" class="form-label">Role</label>
                                                             <select name="role" id="inputState" class="form-select">
-                                                                <option selected>Select Role</option>
-                                                                <option value="admin">Admin</option>
-                                                                <option value="user">User</option>
-                                                                <option value="teacher">Teacher</option>
+                                                                <option value="none">Select Role</option>
+                                                                <option value="admin"
+                                                                    {{ old('status') == 'admin' || request('status') == 'admin' ? 'selected' : '' }}>
+                                                                    Admin</option>
+                                                                <option value="user"
+                                                                    {{ old('status') == 'user' || request('status') == 'user' ? 'selected' : '' }}>
+                                                                    User</option>
+                                                                <option value="teacher"
+                                                                    {{ old('status') == 'teacher' || request('status') == 'teacher' ? 'selected' : '' }}>
+                                                                    Teacher</option>
                                                             </select>
+
+                                                            @error('role')
+                                                                <p class="mb-0"><small
+                                                                        class="text-danger fs-6">{{ $message }}</small></p>
+                                                            @enderror
                                                         </div>
                                                         <div class="col-md-6">
                                                             <label for="inputState" class="form-label">Status</label>
                                                             <select name="status" id="inputState" class="form-select">
-                                                                <option selected>Select status</option>
-                                                                <option value="active">Active</option>
-                                                                <option value="inactive">Inactive</option>
+                                                                <option value="none">Select Status</option>
+                                                                <option value="active"
+                                                                    {{ old('status') == 'active' || request('status') == 'active' ? 'selected' : '' }}>
+                                                                    Active</option>
+                                                                <option value="inactive"
+                                                                    {{ old('status') == 'inactive' || request('status') == 'inactive' ? 'selected' : '' }}>
+                                                                    Inactive</option>
                                                             </select>
+
+                                                            @error('status')
+                                                                <p class="mb-0"><small
+                                                                        class="text-danger fs-6">{{ $message }}</small></p>
+                                                            @enderror
                                                         </div>
                                                         <div class="col-md-12">
 
