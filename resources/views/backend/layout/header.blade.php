49122716cf2211b5b51f3b9d6d96aba467bdaea7
Moshiul <gazimoshiul@gmail.com>
2022-10-12 00:13:31 +0600

    simple-update-1

--- resources/views/backend/layout/header.blade.php
+++ resources/views/backend/layout/header.blade.php
@@ -898,7 +898,7 @@
                                         <span
                                             class="d-none d-xl-inline-block ms-1 fw-medium user-name-text">{{ Auth::user()->name }}</span>
                                         <span
-                                            class="d-none d-xl-block ms-1 fs-12 text-muted user-name-sub-text">{{ Auth::user()->roles[0]->name }}</span>
+                                            class="d-none d-xl-block ms-1 fs-12 text-muted user-name-sub-text">{{ ucwords(str_replace('-',' ',Auth::user()->roles[0]->name)) }}</span>
                                     </span>
                                 </span>
                             </button>
